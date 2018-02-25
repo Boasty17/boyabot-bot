8e8db822f96f6e72e2587c26b08154666836d506
Boasty17 <36826729+Boasty17@users.noreply.github.com>
2018-02-25 22:17:22 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,8 @@
+{
+  "name":BoyaBot",
+    "version": "1.0.0",
+      "description": "A Good bot for everyone!",
+        "main": "bot.js",
+          "author": "Boasty",
+            "dependencies": {}
+            }
